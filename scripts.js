3f769750ca4d4266242808f875bcbf0048555ee4
AnnaWijetunga <annawijetunga@gmail.com>
2020-07-03 09:56:03 -0400

    Function red effect to alter the photo

--- scripts.js
+++ scripts.js
@@ -75,7 +75,14 @@ function takePhoto() {
 
 // filters
 // get the pixels out of the canvas, mess with 'em, put them back in
-
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
 
 // call the function
 getVideo();
